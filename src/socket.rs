5f0a9161232e48e5906ccccd8f6409e6756513c1
Genryu Inami <genryu7@gmail.com>
2025-08-07 21:05:14 +0900

    refactor: clean up imports and improve code formatting in HttpSocket implementation

--- src/socket.rs
+++ src/socket.rs
@@ -1,6 +1,6 @@
 use crate::url::Socket;
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
-use std::io::{Write, BufRead, BufReader, Read};
 
 #[derive(Debug)]
 pub struct HttpSocket {
@@ -22,7 +22,8 @@ impl Socket for HttpSocket {
         let address = format!("{}:{}", host, port);
         match TcpStream::connect(&address) {
             Ok(stream) => {
-                let cloned_stream = stream.try_clone()
+                let cloned_stream = stream
+                    .try_clone()
                     .map_err(|e| format!("Failed to clone stream: {}", e))?;
                 self.reader = Some(BufReader::new(cloned_stream));
                 self.stream = Some(stream);
@@ -34,12 +35,10 @@ impl Socket for HttpSocket {
 
     fn send(&mut self, data: &[u8]) -> Result<(), String> {
         match self.stream.as_mut() {
-            Some(stream) => {
-                match stream.write_all(data) {
-                    Ok(()) => Ok(()),
-                    Err(e) => Err(format!("Failed to send data: {}", e)),
-                }
-            }
+            Some(stream) => match stream.write_all(data) {
+                Ok(()) => Ok(()),
+                Err(e) => Err(format!("Failed to send data: {}", e)),
+            },
             None => Err("Not connected to any server".to_string()),
         }
     }
